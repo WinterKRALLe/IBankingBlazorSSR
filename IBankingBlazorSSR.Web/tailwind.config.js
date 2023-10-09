dc2a96c35efa1a5050bc52934f2761293a2a9ffa
WinterKRALLe <winterdbek@protonmail.com>
2023-10-09 14:31:16 +0200

    updated styles; added font

--- IBankingBlazorSSR.Web/tailwind.config.js
+++ IBankingBlazorSSR.Web/tailwind.config.js
@@ -1,9 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './Components/*.razor',
+        './Components/**/*.razor'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                skin: {
+                    base: 'var(--color-base)',
+                    base2: 'var(--color-base2)',
+                    muted: 'var(--color-muted)',
+                    inverted: 'var(--color-inverted)',
+                    fill: 'var(--color-fill)',
+                    fill_accent: 'var(--color-fill-accent)',
+                    button_accent: 'var(--color-button-accent)',
+                    button_accent_hover: 'var(--color-button-accent-hover)',
+                    button_muted: 'var(--color-button-muted)',
+                },
+            },
+            textColor: {
+                skin: {
+                    base: 'var(--color-base)',
+                    base2: 'var(--color-base2)',
+                    muted: 'var(--color-muted)',
+                    inverted: 'var(--color-inverted)',
+                    accent: 'var(--color-text-accent)',
+                },
+            },
+            backgroundColor: {
+                skin: {
+                    base: 'var(--color-base)',
+                    base2: 'var(--color-base2)',
+                    muted: 'var(--color-muted)',
+                    inverted: 'var(--color-inverted)',
+                    fill: 'var(--color-fill)',
+                    fill_accent: 'var(--color-fill-accent)',
+                    button_accent: 'var(--color-button-accent)',
+                    button_accent_hover: 'var(--color-button-accent-hover)',
+                    button_muted: 'var(--color-button-muted)',
+                },
+            },
+        },
+    },
+    plugins: [],
 }
-
